4e6dc2d72111ac5c449eb6e6bdd1b379d6b4f2cf
Henry Senerpida Matidios <henrymatids@gmail.com>
2016-07-26 16:20:57 +0800

    admin page update

--- application/modules/user_management/views/user_management.php
+++ application/modules/user_management/views/user_management.php
@@ -1,5 +1,64 @@
 <div id="userManagement">
-    <div class="userManagementSampleComponent">
-        
+    <div class="row"></div>
+    <div class="col ">
+        <div class="row">
+            <button class="modal-trigger adduser-btn btn red darken-3" style="margin-left:auto; margin-right:auto; padding-left:10px; padding-right:10px;"><i class="fa fa-plus" aria-hidden="true"></i> Add User</button>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col s12 l12">
+            <div class="userManagementTable">
+            </div>
+        </div>
+    </div>
+    <div class="prototype">
+        <table>
+            <tr class="userManagementEntry">
+                <td class="username"></td>
+                <td class="firstname"></td>
+                <td class="middlename"></td>
+                <td class="lastname"></td>
+                <td><button class="modal-trigger edit-btn btn blue darken-1" style="margin-left:auto; margin-right:auto; padding-left:10px; padding-right:10px;"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Edit</button></td>
+            </tr>
+        </table>
+    </div>
+    <!--Modal Structure-->
+    <div id="modal1" class="modal modal-fixed-footer">
+        <form  method="POST" action="" id="informationForm">    
+            <input field_name="userInfoAccountType" value="" type="hidden" class="validate">
+            <input field_name="userInfoStatus" value="" type="hidden" class="validate">
+            <div class="modal-content">
+                <h5 class="modal-title">User Information</h5>
+                <div class="input-field col s12 m6 l6">              
+                    <input field_name="userInfoUsername" value="" type="text" class="validate" data-error=".errorTxt7">
+                    <label for="userInfoUsername" class="">Username</label>
+                </div>
+                <div class="input-field col s12 m6 l6">              
+                    <input field_name="userInfoPassword" value="" type="password" class="validate" data-error=".errorTxt7">
+                    <label for="userInfoPassword" class="">Password</label>
+                </div>
+                <div class="input-field col s12 m6 l6">              
+                    <input field_name="userInfoFirstname" value="" type="text" class="validate">
+                    <label for="userInfoFirstname" class="">Firstname</label>
+                </div>
+                <div class="input-field col s12 m6 l6">              
+                    <input field_name="userInfoMiddlename" value="" type="text" class="validate">
+                    <label for="userInfoMiddlename" class="">Middlename</label>
+                </div>
+                <div class="input-field col s12 m6 l6">              
+                    <input field_name="userInfoLastname" value="" type="text" class="validate">
+                    <label for="userInfoLastname" class="">Lastname</label>
+                </div>  
+                <div class="input-field col s12 m6 l6">              
+                    <input field_name="userInfoEmail" value="" type="email" class="validate">
+                    <label for="userInfoEmail" class="">E-mail</label>  
+                </div> 
+            </div>
+            <div class="modal-footer">
+                <button href="#!" class=" modal-action modal-close waves-effect waves-green btn-flat">Cancel</button>
+                <button href="#!" class="modal-action waves-effect waves-green btn-flat" id="createBtn">Create</button>
+                <button href="#!" class=" modal-action waves-effect waves-green btn-flat" id="updateBtn">Update</button>
+            </div>
+        </form>
     </div>
 </div>
--- application/modules/user_management/views/user_management_script.php
+++ application/modules/user_management/views/user_management_script.php
@@ -7,21 +7,71 @@
     
     /*Module Object*/
     systemApplication.module.userManagement = function(){
-        var sampleModule = this;//instance of the module
-        var moduleBody = sampleModule.body = $("#userManagement");
-        load_component("sample_component", function(){
-           new SampleComponent(moduleBody.find(".userManagementSampleComponent"));
+        var userManagement = this;//instance of the module
+        var moduleBody = userManagement.body = $("#userManagement");
+        load_component("table_component", function(){
+            var columnConfiguration = [{
+                description : "Username",
+                table_column : "account__username",
+                default_sort : 1
+            },{
+                description : "Firstname",
+                table_column : "account_information__first_name",
+                default_sort : 1
+            },{
+                description : "Middlename",
+                table_column : "account_information__middle_name",
+                default_sort : 0
+            },{
+                description : "Lastname",
+                table_column : "account_information__last_name",
+                default_sort : 2
+            },{
+                description : "Action"
+            }];
+            var filterConfiguration = [{
+                    description : "Search",
+                    table_column : "like__account_information__first_name",
+                    type : "text"
+            }];
+            var resultConfiguration = {
+                result_link : api_url("C_account/retrieveAccount"),
+                success : listCategory,
+                limit : 5
+            };
+            userManagement.tableCategory = new TableComponent(moduleBody.find('.userManagementTable'), resultConfiguration, columnConfiguration, filterConfiguration);
         });
-        load_component("sample_component", function(){
-           new SampleComponent(moduleBody.find(".userManagementSampleComponent"));
+       
+        function listCategory(data){
+            for(var x = 0; x < data.length; x++){
+                var newEntry = moduleBody.find(".prototype .userManagementEntry").clone();
+                newEntry.find(".username").text(data[x]["username"]);
+                newEntry.find(".firstname").text(data[x]["first_name"]);
+                newEntry.find(".middlename").text(data[x]["middle_name"]);
+                newEntry.find(".lastname").text(data[x]["last_name"]);
+                newEntry.find('.edit-btn').click(function(){
+                    $('#createBtn').hide();
+                    $('#modal1').openModal();
+                });
+                        
+                userManagement.tableCategory.addEntry(newEntry);
+            }
+        }      
+        moduleBody.find('.adduser-btn').click(function(){
+            $('#updateBtn').hide();
+            $('#modal1').openModal();
         });
-        load_component("sample_component", function(){
-           new SampleComponent(moduleBody.find(".userManagementSampleComponent"));
+        moduleBody.find('.edit-btn').click(function(){
+            changeFieldName('update', moduleBody.find('#informationForm'));
+            moduleBody.find('#informationForm').attr("action","api_url(C_account/updateAccount)");
         });
-        
-        
+        moduleBody.find('.adduser-btn').click(function(){
+            console.log(moduleBody.find('#informationForm'));
+            changeFieldName('create', moduleBody.find('#informationForm'));
+            moduleBody.find('#informationForm').attr("action","api_url(C_account/createAccount)");
+        });    
     };
-        
+      
     $(document).ready(function(){
         var userManagement = new systemApplication.module.userManagement();
     });
